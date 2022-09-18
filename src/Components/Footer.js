cbc2eab6972fdefb897ea9e9ceb23d72416fd857
AlekhineAlexander <94918214+AlekhineAlexander@users.noreply.github.com>
2022-09-18 19:06:40 +0200

    update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,24 +9,22 @@ const footer = (props) => {
         <div className="row">
           <div className="col-12 text-center">
             <Link to="index.html" className="logo foo-logo">
-              <img src={require("../assets/images/logo.png")} alt="" />
+              <img src={require("../assets/images/logo1.png")} alt="" />
             </Link>
             <nav className="foo-nav">
               <ul>
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link to="/">Strona główna</Link>
                 </li>
                 <li>
-                  <Link to="/about">About</Link>
+                  <Link to="/about">O nas</Link>
+                </li>
+
+                <li>
+                  <Link to="/blog">Cennik</Link>
                 </li>
                 <li>
-                  <Link to="/feature">feature</Link>
-                </li>
-                <li>
-                  <Link to="/services">service</Link>
-                </li>
-                <li>
-                  <Link to="/contact">Contact</Link>
+                  <Link to="/contact">Kontakt</Link>
                 </li>
               </ul>
             </nav>
@@ -34,19 +32,14 @@ const footer = (props) => {
               <Link to="#">
                 <i className="ti-facebook"></i>
               </Link>
-              <Link to="#">
-                <i className="ti-twitter-alt"></i>
-              </Link>
-              <Link to="#">
-                <i className="ti-vimeo-alt"></i>
-              </Link>
+
               <Link to="#">
                 <i className="ti-instagram"></i>
               </Link>
             </div>
             <p className="copyright">
               &copy; COPYRIGHT {`${new Date().getFullYear()} `}
-              <a href="https://themeies.com">themeies.com.</a> ALL RIGHTS
+              <a href="https://google.com">geeko.software</a> ALL RIGHTS
               RESERVED.
             </p>
           </div>
